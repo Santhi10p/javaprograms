56c5907db7d471b86ab3fbc6fe198f13232e5ee7
Santhi10p <112076530+Santhi10p@users.noreply.github.com>
2025-01-29 18:09:28 +0530

    Add files via upload

---
+++ area1.java
@@ -0,0 +1,12 @@
+package printer;
+
+public class area1 {
+
+	public static void main(String[] args) {
+		int a=50,b=40,area;
+		System.out.println("area of square is : "+ (a*b));
+		System.out.println("area of rectangle is :"+ (0.5*a*b));
+		System.out.println("area of circle is :"+ (3.14*a*a));
+	}
+
+}
