3c5821c05aac5893914218a8c7ef7fd1fe491509
Santhi10p <112076530+Santhi10p@users.noreply.github.com>
2025-01-29 18:25:41 +0530

    Update print1.java

--- print1.java
+++ print1.java
@@ -1,4 +1,15 @@
-class print1
+package java1;
+import java.util.Scanner;
+public class add 
 {
-	
-}
+		public static void main(String[] args)
+			{
+			int i,res=0;
+			for(i=1;i<=10;i++)
+				{
+					res=i+res;
+				}
+			System.out.println(res);
+		}
+	}
+
